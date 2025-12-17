bed6a90c4fbc8cfb79f0668ffc23bf5aa5479775
Mohamed Ahmed <20201700656@cis.asu.edu.ed>
2025-12-17 08:38:13 +0200

    Add project files.

---
+++ Food Delivery/Program.cs
@@ -0,0 +1,45 @@
+
+namespace Food_Delivery
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen(); 
+            #endregion
+
+
+
+            var app = builder.Build();
+
+            #region Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run(); 
+            #endregion
+       
+        
+        
+        }
+    }
+}
